79ba8d134e587b81de07151ef587dac512a78534
Manasi Singh <30742519+manameme@users.noreply.github.com>
2024-10-25 23:42:10 +0800

    Update User.java

--- User.java
+++ User.java
@@ -3,23 +3,29 @@ public class User {
     private String password;
     private String role;
     
+    // Constructor
+    public User(String userID, String password, String role) {
+        this.userID = userID;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Login method using .equals for string comparison
     public boolean login(String user, String pass) {
-        if (userID == user && password == pass){
-            return true;
-        }
-        else return false;
-    };
-    
-    public boolean changePassword(String oldPass, String newPass){
-        if (password == oldPass) {
+        return userID.equals(user) && password.equals(pass);
+    }
+
+    // Password change method using .equals for string comparison
+    public boolean changePassword(String oldPass, String newPass) {
+        if (password.equals(oldPass)) {
             password = newPass;
             return true;
         }
-        else return false;
+        return false;
     }
-    
+
+    // Get role method
     public String getRole() {
-    	return role;
+        return role;
     }
-    
 }
