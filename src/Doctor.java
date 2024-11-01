c1d33bee9e7d722b63017e9e82c4791149213071
Katstengels <167674845+Katstengels@users.noreply.github.com>
2024-11-01 15:22:54 +0800

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 public class Doctor extends Staff{
     private Schedule[][] schedule = new Schedule[7][7];    
     
@@ -16,57 +18,96 @@ public class Doctor extends Staff{
     }
 
     public void setAvailability() {
-		int dChoice, tChoice, avail;
-		Scanner sc = new Scanner(System.in);
-		do {
-			for (int i=0; i<7; i++) {
-				System.out.format("%d. %s", i+1, schedule[i][1].getDate());
-			}
-			System.out.println("Choose date: ");
-			dChoice = sc.nextInt();
+	int dChoice, tChoice, avail;
+	Scanner sc = new Scanner(System.in);
+	do {
+		for (int i=0; i<7; i++) {
+			System.out.format("%d. %s\n", i+1, schedule[i][1].getDate());
+		}
+		System.out.println("8. Back");
+		System.out.println("Choose date: ");
+		dChoice = sc.nextInt();
 			
-			if (dChoice < 8) {
-				do {
-					for(int j=0; j<7; j++) {
-						System.out.format("%d. %s", j+1, schedule[dChoice-1][j].getTime());
-						}
-					System.out.println("Choose time: ");
-					tChoice = sc.nextInt();
+		if (dChoice > 0 && dChoice < 8) {
+			do {
+				System.out.format("%s\n", schedule[dChoice-1][1].getDate());
+				for(int j=0; j<7; j++) {
+					System.out.format("%d. %s\n", j+1, schedule[dChoice-1][j].getTime());
+					}
+				System.out.println("8. Back");
+				System.out.println("Choose time: ");
+				tChoice = sc.nextInt();
 					
-					if (tChoice < 8) {
-						do {
-							System.out.println("Indicate availability:");
-							System.out.println("1. Available");
-							System.out.println("2. Not available");
-							avail = sc.nextInt();
+				if (tChoice > 0 && tChoice < 8) {
+					do {
+						System.out.println("Indicate availability:");
+						System.out.println("1. Available");
+						System.out.println("2. Not available");
+						System.out.println("3. Back");
+						avail = sc.nextInt();
 						
-							switch(avail) {
-							case 1:
-								schedule[dChoice-1][tChoice-1].changeAvail(true);
-								break;
-							case 2:
-								schedule[dChoice-1][tChoice-1].changeAvail(false);
-								break;
-							default:
-								System.out.println("Invalid choice! Please enter again.");
-								break;
-							}
-						} while (avail>2);
-					}
-					else {
-						System.out.println("Invalid time! Please enter again.");
-					}
+						switch(avail) {
+						case 1:
+							schedule[dChoice-1][tChoice-1].changeAvail(true);
+							System.out.format("Available on %s %s\n", schedule[dChoice-1][tChoice-1].getDate(), schedule[dChoice-1][tChoice-1].getTime());
+							break;
+						case 2:
+							schedule[dChoice-1][tChoice-1].changeAvail(false);
+							System.out.format("Unvailable on %s %s\n", schedule[dChoice-1][tChoice-1].getDate(), schedule[dChoice-1][tChoice-1].getTime());
+							break;
+						case 3:
+							break;
+						default:
+							System.out.println("Invalid choice! Please enter again.");
+							break;
+						}
+					} while (avail > 3 || avail < 1);
+				}
+				else if(tChoice > 8 || tChoice < 1){
+					System.out.println("Invalid time! Please enter again.");
+				}
 					
-				} while (tChoice > 7);
-			}
+			} while (tChoice != 8);
+		}
 				
-			else {
-					System.out.println("Date invalid! Please enter again.");
-			}
-		} while (dChoice > 7); 
-	}
-    //Feel free to add all the appointments and other stuff here
+		else if (dChoice > 8 || dChoice < 1){
+				System.out.println("Date invalid! Please enter again.");
+		}
+	} while (dChoice != 8); 
+    }
+	
+	
+    public void getAllAvail(){
+	int dChoice;
+	Scanner sc = new Scanner(System.in);
+	do {
+		for (int i=0; i<7; i++) {
+			System.out.format("%d. %s\n", i+1, schedule[i][1].getDate());
+		}
+		System.out.println("8. Back");
+		System.out.println("Choose date: ");
+		dChoice = sc.nextInt();
+			
+		if (dChoice < 8 && dChoice > 0) {
+			System.out.format("%s\n", schedule[dChoice-1][1].getDate());
+			for(int j=0; j<7; j++) {
+				System.out.format("%d. %s : ", j+1, schedule[dChoice-1][j].getTime());
+				if (schedule[dChoice-1][j].getAvail()) System.out.println("Available");
+				else System.out.println("Unavailable");
+			}					
+		}
+				
+		else if (dChoice > 8|| dChoice < 1){
+			System.out.println("Date invalid! Please enter again.");
+		}
+	} while (dChoice != 8 );
+    }
 
-
+    public getAvailability(Calendar time){
+	    for (int i=0; i<8; i++){
+		    for (int j=0; j<8; j++){
+			    if (time == schedule[i][j].getDateTime()) return schedule[i][j].getAvail;
+		    }
+	    }
+    }
 }
-
