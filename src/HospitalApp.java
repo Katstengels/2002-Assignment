74a3d18b969518d72855003fb049fefd36cc54ed
Katstengels <167674845+Katstengels@users.noreply.github.com>
2024-11-01 23:26:25 +0800

    Create HospitalApp.java

---
+++ src/HospitalApp.java
@@ -0,0 +1,277 @@
+package newAssignment;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HospitalApp {
+
+	public static void main(String[] args) {
+		
+        PatientInv pList = PatientInv.getInstance();
+		StaffInv sList = StaffInv.getInstance();
+		
+		ArrayList<Staff> staffList = sList.copyStaffList(); //Use members in staff list instance from StaffInv
+		ArrayList<Patient> patientList = pList.copyPatientList();
+		ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+		
+		Scanner sc = new Scanner(System.in);
+		String username, password;
+		User user = null;
+		boolean loggedIn = false;
+		Calendar calendar = Calendar.getInstance();
+		Date now = calendar.getTime();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM YYYY");
+		
+		do {
+			do {
+				System.out.println("Enter Username: ");
+				username = sc.nextLine();
+				System.out.println("Enter Password: ");
+				password = sc.nextLine();
+				
+				for (Staff s : staffList) {
+					loggedIn = s.login(username, password);
+					if (loggedIn) {
+						user = s;
+						break;
+					}
+				}
+				
+				if (!loggedIn) {
+				 	for (Patient p : patientList) {
+						loggedIn = p.login(username, password);
+						if (loggedIn) {
+							user = p;
+							break;
+						}
+				 	}
+				}   
+				
+				if (!loggedIn) {
+					System.out.println("Username or password incorrect! Please try again.");
+				}
+				
+			} while (!loggedIn);
+			
+			System.out.println("Welcome!");
+			
+			switch (user.getRole()) {
+			case "Doctor":
+				Doctor d = (Doctor) user;
+				int patSelect, aptSelect, patAmount, aptAmount;
+				Patient pat;
+				Appointment apt;
+				int choice;
+				
+				do {
+					System.out.println("1. View Patient Medical Records");
+					System.out.println("2. Update Patient Medical Records");
+					System.out.println("3. View Personal Schedule");
+					System.out.println("4. Set Availability for Appointments");
+					System.out.println("5. Accept or Decline Appointment Requests");
+					System.out.println("6. View Upcoming Appointments");
+					System.out.println("7. Record Appointment Outcome");
+					System.out.println("8. Logout");
+					choice = sc.nextInt();
+					
+					switch(choice) {
+					case 1:
+						patAmount = 0;
+						for (Patient p : patientList) {
+							patAmount++;
+							System.out.println((patAmount) + ". " + p.getName());
+						}
+						
+						do {					
+							System.out.println("Select patient:");
+							patSelect = sc.nextInt();
+							if (patSelect > patAmount) System.out.println("Invalid patient! Please enter again.");
+						} while (patSelect > patAmount);
+						
+						pat = patientList.get(patAmount-1);
+						System.out.println("Name		: " + pat.getName());
+						System.out.println("DOB		: " + dateFormat.format(pat.getDOB()));
+						System.out.println("Email		: " + pat.getEmail());
+						System.out.println("Contact no.	: " + pat.getContactNum());
+						System.out.println("Blood type	: " + pat.getBloodType());
+						System.out.println("Previous appointment outcomes:");
+						for (Appointment a : appointmentList) {
+							if (pat.getName().equals(a.getPatient())) {
+								System.out.println(a.getDate() + ": " + a.getOutcome());
+							}
+						}
+						System.out.println();
+						break;
+						
+					case 2:
+						patAmount = 0;
+						aptAmount = 0;
+						for (Patient p : patientList) {
+							patAmount++;
+							System.out.println((patAmount) + ". " + p.getName());
+						}
+						
+						do {					
+							System.out.println("Select patient:");
+							patSelect = sc.nextInt();
+							if (patSelect > patAmount) System.out.println("Invalid patient! Please enter again.");
+						} while (patSelect > patAmount);
+						
+						pat = patientList.get(patAmount-1);
+						
+						//need patient update method
+						
+						System.out.println();
+						break;
+						
+					case 3:
+						d.getAllAvailability();
+						System.out.println();
+						break;
+					
+					case 4:
+						d.setAvailability();
+						System.out.println();
+						break;
+						
+					case 5:
+						aptAmount = 0;
+						for (Appointment a : appointmentList) {
+							if(a.getDoctor().equals(d.getName())) {
+								aptAmount++;
+								System.out.println(aptAmount + ". " + a.getID() + ": " + a.getDate() + " " + a.getTime());
+							}
+						}
+						
+						if (aptAmount != 0) {
+							do {					
+								System.out.println("Select appointment:");
+								aptSelect = sc.nextInt();
+								if (aptSelect > aptAmount) System.out.println("Invalid appointment! Please enter again.");
+							} while (aptSelect > aptAmount);
+							
+							apt = appointmentList.get(aptSelect-1);
+							
+							int acc;
+							
+							System.out.println("1. Accept appointment");
+							System.out.println("2. Reject appointment");
+							do {
+								System.out.println("Enter selection: ");
+								acc = sc.nextInt();
+								if (acc > 2) System.out.println("Invalid selection! Please enter again.");
+							} while (acc>2);
+							
+							switch (acc) {
+							case 1:
+								apt.updateStatus("Confirmed");
+								d.addAppointment(apt);
+								System.out.println("Appointment accepted.");
+								break;
+							case 2:
+								apt.updateStatus("Rejected");
+								System.out.println("Appointment rejected.");
+								break;
+							}
+						}
+						
+						else System.out.println("No appointment found!");
+						System.out.println();
+						break;
+						
+					case 6:
+						System.out.println("Upcoming appointments: ");
+						
+						aptAmount = 0;
+						
+						for (Appointment a : appointmentList) {
+							if (a.getDoctor().equals(d.getName()) && a.getDateTime().after(now)) {
+								System.out.println();
+								System.out.println("Appointment ID	: " + a.getID());
+								System.out.println("Date			: " + a.getDate());
+								System.out.println("Time 			: " + a.getTime());
+								System.out.println("Patient			: " + a.getPatient());
+								aptAmount++;
+							}
+						}
+						
+						if (aptAmount == 0) System.out.println("No appointment found!");
+						System.out.println();
+						break;
+					
+					case 7:
+						patAmount = 0;
+						aptAmount = 0;
+						for (Patient p : patientList) {
+							patAmount++;
+							System.out.println((patAmount) + ". " + p.getName());
+						}
+						
+						do {					
+							System.out.println("Select patient:");
+							patSelect = sc.nextInt();
+							if (patSelect > patAmount) System.out.println("Invalid patient! Please enter again.");
+						} while (patSelect > patAmount);
+						
+						pat = patientList.get(patAmount-1);
+						for (Appointment a : appointmentList) {
+							if (pat.getName().equals(a.getPatient()) && d.getName().equals(a.getDoctor())) {
+								aptAmount++;
+								System.out.println((aptAmount) + ". " + a.getDate());
+							}
+						}
+						if (aptAmount != 0) {
+							do {					
+								System.out.println("Select appointment:");
+								aptSelect = sc.nextInt();
+								if (aptSelect > aptAmount) System.out.println("Invalid appointment! Please enter again.");
+							} while (aptSelect > aptAmount);
+							
+							apt = appointmentList.get(aptSelect-1);
+							
+							apt.updateOutcome(sc.nextLine());
+						}
+						
+						else System.out.println("No appointment found!");
+						System.out.println();
+						break;
+						
+					case 8:
+						System.out.println("Logging out...");
+						sc.nextLine();
+						loggedIn = false;
+						System.out.println();
+						break;
+						
+					default:
+						System.out.println("Invalid choice! Please enter again.");
+						break;
+					}
+				} while (choice != 8);
+				
+				
+				break;
+				
+			case "Pharmacist":
+				System.out.println("Pharmacist");
+				
+				break;
+				
+			case "Administrator":
+				System.out.println("Admin");
+				
+				break;
+				
+			case "Patient":
+				System.out.println("Patient");
+				
+				break;
+			}
+			
+		} while (!loggedIn); //go back to login page
+	}
+
+}
