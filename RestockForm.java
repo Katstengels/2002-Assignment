4d9edec6c3e1991acf76bdaefbfaa55880d21794
HighlandK <165983517+HighlandK@users.noreply.github.com>
2024-11-08 17:08:12 +0800

    Add files via upload

---
+++ RestockForm.java
@@ -0,0 +1,18 @@
+package project2002;
+
+public class RestockForm {
+	private int restockID;
+	private static int restockIDCounter=0;
+    private String medicationName;
+    private int restockAmount;
+    public enum status {
+        Pending_Admin, Fulfilled
+    }
+    private String restockStatus;
+
+
+    public RestockForm(){
+    	
+    }
+    //
+}
