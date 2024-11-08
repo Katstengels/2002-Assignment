233163b4ea7c6cd2b262c573d6cea6316b7b503c
Katstengels <167674845+Katstengels@users.noreply.github.com>
2024-11-08 11:27:21 +0800

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -2,10 +2,12 @@ package src;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class Patient extends User {
+	private Calendar calendar = Calendar.getInstance();
     private String patientID;
     private String name;
     private Date dateOfBirth;
@@ -13,6 +15,7 @@ public class Patient extends User {
     private String email;
     private String contactNum;
     private String bloodType;
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
     // Constructor
     public Patient(String userID, String password, String role, String patientID, String name, Date dateOfBirth,
@@ -50,7 +53,74 @@ public class Patient extends User {
         return contactNum;
     }
 
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    public void updatePart() {
+    	int choice, d, m, y;
+    	String newPart;
+    	
+    	System.out.println("Choose particular to change: ");
+    	System.out.println("1. Name");
+    	System.out.println("2. Date of Birth");
+    	System.out.println("3. Gender");
+    	System.out.println("4. Email");
+    	System.out.println("5. Contact number");
+    	System.out.println("6. Blood type");
+    	
+    	do {
+    		System.out.println("Enter selection: ");
+    		choice = sc.nextInt();
+    		
+    		if (choice>6) System.out.println("Invalid selection! Please enter again.");
+    	} while (choice>6);
+    	
+    	switch (choice){
+    	case 1:
+    		newPart = sc.nextLine();
+    		name = newPart;
+    		break;
+    		
+    	case 2:
+    		do {
+        		System.out.println("Enter date: ");
+        	    d = sc.nextInt();
+        	    
+        	    if (d>31) System.out.println("Invalid date! Please enter again");
+        	} while (d>31);
+        	
+        	do {
+        		System.out.println("Enter month: ");
+        	    m = sc.nextInt();
+        	    
+        	    if (m>12) System.out.println("Invalid month! Please enter again");
+        	} while (m>12);
+        	
+        	System.out.println("Enter year: ");
+    	    y = sc.nextInt();
+        	
+        	calendar.set(y, m, d);
+        	dateOfBirth = calendar.getTime();
+    		break;
+    		
+    	case 3:
+    		newPart = sc.nextLine();
+    		gender = newPart;
+    		break;
+    		
+    	case 4:
+    		newPart = sc.nextLine();
+    		email = newPart;
+    		break;
+    		
+    	case 5:
+    		newPart = sc.nextLine();
+    		contactNum = newPart;
+    		break;
+    		
+    	case 6:
+    		newPart = sc.nextLine();
+    		bloodType = newPart;
+    		break;
+    	}
+    }
 
 
 
