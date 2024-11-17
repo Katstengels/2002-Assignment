963676e399feba00898f688bb461d523972b6255
Katstengels <167674845+Katstengels@users.noreply.github.com>
2024-11-17 20:36:47 +0800

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -77,16 +77,12 @@ public class Patient extends User {
     }
 
 	public void updatePart() {
-		int choice = 0, d = 0, m = 0, y = 0;
+		int choice = 0;
 		String newPart;
 
 		System.out.println("Choose particular to change: ");
-		System.out.println("1. Name");
-		System.out.println("2. Date of Birth");
-		System.out.println("3. Gender");
-		System.out.println("4. Email");
-		System.out.println("5. Contact number");
-		System.out.println("6. Blood type");
+		System.out.println("1. Email");
+		System.out.println("2. Contact number");
 
 		// Input validation for 'choice'
 		do {
@@ -97,7 +93,7 @@ public class Patient extends User {
 			}
 			choice = sc.nextInt();
 
-			if (choice > 6 || choice < 1) {
+			if (choice > 2 || choice < 1) {
 				System.out.println("Invalid selection! Please enter again.");
 			}
 		} while (choice > 6 || choice < 1);
@@ -106,80 +102,17 @@ public class Patient extends User {
 
 		switch (choice) {
 			case 1:
-				System.out.println("Enter new name: ");
-				newPart = sc.nextLine();
-				name = newPart;
-				break;
-
-			case 2:
-				// Input validation for 'd' (day)
-				do {
-					System.out.println("Enter day: ");
-					while (!sc.hasNextInt()) {
-						System.out.println("Invalid input! Please enter an integer.");
-						sc.next(); // Consume the invalid input
-					}
-					d = sc.nextInt();
-
-					if (d > 31 || d < 1) {
-						System.out.println("Invalid date! Please enter again.");
-					}
-				} while (d > 31 || d < 1);
-
-				// Input validation for 'm' (month)
-				do {
-					System.out.println("Enter month: ");
-					while (!sc.hasNextInt()) {
-						System.out.println("Invalid input! Please enter an integer.");
-						sc.next(); // Consume the invalid input
-					}
-					m = sc.nextInt();
-
-					if (m > 12 || m < 1) {
-						System.out.println("Invalid month! Please enter again.");
-					}
-				} while (m > 12 || m < 1);
-
-				// Input validation for 'y' (year)
-				System.out.println("Enter year: ");
-				while (!sc.hasNextInt()) {
-					System.out.println("Invalid input! Please enter an integer.");
-					sc.next(); // Consume the invalid input
-				}
-				y = sc.nextInt();
-
-				calendar.set(y, m - 1, d); // Adjust month by subtracting 1 (Calendar month starts from 0)
-				dateOfBirth = calendar.getTime();
-
-				System.out.println("DOB edited ");
-				break;
-
-			case 3:
-				System.out.println("Enter new gender: ");
-				newPart = sc.nextLine();
-				gender = newPart;
-				System.out.println("Gender edited ");
-				break;
-
-			case 4:
 				System.out.println("Enter new email: ");
 				newPart = sc.nextLine();
 				email = newPart;
 				System.out.println("Email edited ");
 				break;
 
-			case 5:
+			case 2:
 				System.out.println("Enter new contact number: ");
 				newPart = sc.nextLine();
 				contactNum = newPart;
 				System.out.println("Contact number edited ");
-				break;
-
-			case 6:
-				System.out.println("Enter new blood type: ");
-				newPart = sc.nextLine();
-				bloodType = newPart;
-				System.out.println("Blood type edited ");
 				break;
 		}
 	}
